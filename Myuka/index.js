1959e6677b30b8beb80f2ab5065e5f481bd3521b
Kévin Vendome <kevinvendome@hotmail.com>
2022-02-05 01:19:39 +0100

    final version

---
+++ Myuka/index.js
@@ -0,0 +1,71 @@
+
+const sr = ScrollReveal({
+    reset : true,
+});
+
+sr.reveal('#top',{
+    origin: 'top',
+    duration : 2000,
+    distance : '200px',
+
+});
+sr.reveal('.navbar',{
+    origin: 'right',
+    duration : 6000,
+    distance : '200px',
+
+});
+sr.reveal('#selfie',{
+    origin: 'left',
+    duration : 3000,
+    distance : '200px',
+
+});
+sr.reveal('#slogan',{
+    origin: 'right',
+    duration : 3500,
+    distance : '200px',
+
+});
+sr.reveal('.cereale',{
+    origin: 'left',
+    duration : 4000,
+    distance : '200px',
+
+});
+sr.reveal('.ingredient',{
+    origin: 'right',
+    duration : 3500,
+    distance : '200px',
+
+});
+sr.reveal('#source',{
+    origin: 'bottom',
+    duration : 2000,
+    distance : '200px',
+
+});
+sr.reveal('.blé',{
+    origin: 'left',
+    duration : 4000,
+    distance : '300px',
+
+});
+sr.reveal('.chocolat',{
+    origin: 'top',
+    duration : 8000,
+    distance : '100px',
+
+});
+sr.reveal('.vanille',{
+    origin: 'right',
+    duration : 6000,
+    distance : '400px',
+
+});
+sr.reveal('.pdp',{
+    origin: 'right',
+    duration : 3000,
+    distance : '200px',
+
+});
